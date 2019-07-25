3e6769abcb007103f85dcd6e38984e63f1f2606e
simple_scout <vitale232@gmail.com>
2019-07-25 08:11:10 -0400

    Add method to convert DriveTimeNodes to polygon

--- database/functions.py
+++ database/functions.py
@@ -1,6 +1,14 @@
-from django.db import connection
+import math
 
-from routing.models import DriveTimeNode, WaysVerticesPgr
+from django.db import connection
+import numpy as np
+import shapely.geometry as geometry
+from shapely.ops import cascaded_union, polygonize
+from shapely.wkt import loads
+from scipy.spatial import Delaunay
+
+
+from routing.models import DriveTimeNode, WaysVerticesPgr, DriveTimePolygon
 
 
 class DriveTime:
@@ -44,3 +52,43 @@ class DriveTime:
             ) for row in self.rows
         ]
         return self.models
+    
+    def to_polygon(self, alpha=1, drive_time_query=None):
+        points = [loads(node.the_geom.wkt) for node in self.models]
+        
+        # Skip triangles
+        if len(points) < 4:
+            return geometry.MultiPoint(list(points)).convex_hull
+
+        coords = np.array([point.coords[0] for point in points])
+        tri = Delaunay(coords)
+        triangles = coords[tri.vertices]
+        a = (
+            (triangles[:,0,0] - triangles[:,1,0]) ** 2 +
+            (triangles[:,0,1] - triangles[:,1,1]) ** 2
+        )**0.5
+        b =(
+            ((triangles[:,1,0] - triangles[:,2,0]) ** 2 +
+            (triangles[:,1,1] - triangles[:,2,1]) ** 2) 
+        )** 0.5
+        c = (
+            ((triangles[:,2,0] - triangles[:,0,0]) ** 2 +
+            (triangles[:,2,1] - triangles[:,0,1]) ** 2) 
+        )** 0.5
+        s = ( a + b + c ) / 2.0
+        areas = (s*(s-a)*(s-b)*(s-c)) ** 0.5
+        circums = a * b * c / (4.0 * areas)
+        filtered = triangles[circums < (1.0 / alpha)]
+        edge1 = filtered[:,(0,1)]
+        edge2 = filtered[:,(1,2)]
+        edge3 = filtered[:,(2,0)]
+        edge_points = np.unique(np.concatenate((edge1,edge2,edge3)), axis = 0).tolist()
+        m = geometry.MultiLineString(edge_points)
+        triangles = list(polygonize(m))
+        concave_hull = cascaded_union(triangles)
+        self.drive_time_polygon = DriveTimePolygon.objects.create(
+            drive_time_query=drive_time_query,
+            the_geom=concave_hull.buffer(0.005).wkt
+        )
+        self.drive_time_polygon.save()
+        return self.drive_time_polygon
