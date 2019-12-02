f70a91d038bb8b9b1a902cc90bda1ed90f6cd5c0
simple_scout <vitale232@gmail.com>
2019-12-02 14:04:49 -0500

    Build pebula theme, remove header def

--- angular-app/src/app/components/drive-time-display/bridge-grid/bridge-grid.config.ts
+++ angular-app/src/app/components/drive-time-display/bridge-grid/bridge-grid.config.ts
@@ -10,8 +10,8 @@ export const COLUMNS = columnFactory()
     { prop: 'properties.gtms_structure', sort: true, label: 'Span Structure' },
     { prop: 'properties.gtms_mater', sort: true, label: 'Span Materials' },
     { prop: 'properties.aadt', sort: true, label: 'AADT' },
-  )
-  .header(
-    { rowClassName: 'pbl-groupby-row' },
-    { id: 'pbl-groupby-row', type: 'pbl-groupby-row', label: ' ' },
-    );
+  );
+  // .header(
+  //   { rowClassName: 'pbl-groupby-row' },
+  //   { id: 'pbl-groupby-row', type: 'pbl-groupby-row', label: 'ACE' },
+  // );
