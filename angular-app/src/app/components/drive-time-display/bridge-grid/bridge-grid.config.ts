b6040c1159616ac765c9c46987d7bdd1704b7d87
simple_scout <vitale232@gmail.com>
2019-12-02 17:10:22 -0500

    Change column defs

--- angular-app/src/app/components/drive-time-display/bridge-grid/bridge-grid.config.ts
+++ angular-app/src/app/components/drive-time-display/bridge-grid/bridge-grid.config.ts
@@ -3,14 +3,17 @@ import { columnFactory } from '@pebula/ngrid';
 export const COLUMNS = columnFactory()
   .default({ width: '75px', resize: true, reorder: true})
   .table(
-    { prop: 'id', sort: true, },
+    { prop: 'properties.bin', pIndex: true,  id: 'bin', sort: true, label: 'BIN', },
     { prop: 'properties.inspection', id: 'inspection', sort: true, label: 'Inspected Date' },
-    { prop: 'properties.bin', id: 'bin', sort: true, label: 'BIN', },
     { prop: 'properties.county_name', id: 'countyName', sort: true, label: 'County', },
     { prop: 'properties.primary_owner', id: 'primaryOwner', sort: true, label: 'Owner' },
     { prop: 'properties.gtms_structure', id: 'structure', sort: true, label: 'Span Structure' },
     { prop: 'properties.gtms_mater', id: 'materials', sort: true, label: 'Span Materials' },
+    { prop: 'properties.carried', id: 'carried', sort: true, label: 'Carried Roadway' },
+    { prop: 'properties.crossed', id: 'crossed', sort: true, label: 'Crossed Roadway' },
     { prop: 'properties.aadt', id: 'aadt', sort: true, label: 'AADT' },
+    { prop: 'properties.year_built', id: 'yearBuilt', sort: true, label: 'Year Built' },
+    { prop: 'properties.condition_field', id: 'conditionRating', sort: true, label: 'Condition Rating', type: 'number', }
   );
   // .header(
   //   { rowClassName: 'pbl-groupby-row' },
