1c47a5f94a0525b7c02db8ab29e68f61a48d036c
simple_scout <vitale232@gmail.com>
2019-12-08 14:16:23 -0500

    Resolve conflict

--- angular-app/src/app/components/drive-time-display/bridge-grid/bridge-grid.config.ts
+++ angular-app/src/app/components/drive-time-display/bridge-grid/bridge-grid.config.ts
@@ -4,11 +4,15 @@ export const COLUMNS = columnFactory()
   .default({ width: '75px', resize: true, reorder: true})
   .table(
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 79f6486... Set default column widths in pixels
     { prop: 'zoom_to_icon', minWidth: 50, width: '50px', type: 'zoom_to_icon', pin: 'start', wontBudge: true, reorder: false, },
     { prop: 'properties.bin',  id: 'bin', sort: true, label: 'BIN', },
     { prop: 'properties.inspection', id: 'inspection', width: '120px', sort: true, label: 'Inspected Date', },
     { prop: 'properties.common_name', id: 'commonName', sort: true, width: '150px', label: 'Common Name', },
     { prop: 'properties.county_name', id: 'countyName', sort: true, width: '105px', label: 'County', },
+<<<<<<< HEAD
     { prop: 'properties.primary_owner', id: 'primaryOwner', sort: true, label: 'Owner', },
     { prop: 'properties.gtms_structure', id: 'structure', width: '175px', sort: true, label: 'Span Structure', },
     { prop: 'properties.gtms_mater', id: 'materials', width: '175px', sort: true, label: 'Span Materials' },
@@ -22,13 +26,19 @@ export const COLUMNS = columnFactory()
     { prop: 'properties.inspection', id: 'inspection', sort: true, label: 'Inspected Date', },
     { prop: 'properties.common_name', id: 'commonName', sort: true, label: 'Common Name', },
     { prop: 'properties.county_name', id: 'countyName', sort: true, label: 'County', },
+=======
+>>>>>>> 79f6486... Set default column widths in pixels
     { prop: 'properties.primary_owner', id: 'primaryOwner', sort: true, label: 'Owner', },
-    { prop: 'properties.gtms_structure', id: 'structure', sort: true, label: 'Span Structure', },
-    { prop: 'properties.gtms_mater', id: 'materials', sort: true, label: 'Span Materials' },
-    { prop: 'properties.carried', id: 'carried', sort: true, label: 'Carried Roadway', },
-    { prop: 'properties.crossed', id: 'crossed', sort: true, label: 'Crossed Roadway', },
+    { prop: 'properties.gtms_structure', id: 'structure', width: '175px', sort: true, label: 'Span Structure', },
+    { prop: 'properties.gtms_mater', id: 'materials', width: '175px', sort: true, label: 'Span Materials' },
+    { prop: 'properties.carried', id: 'carried', width: '175px', sort: true, label: 'Carried Roadway', },
+    { prop: 'properties.crossed', id: 'crossed', width: '175px', sort: true, label: 'Crossed Roadway', },
     { prop: 'properties.aadt', id: 'aadt', sort: true, label: 'AADT', },
+<<<<<<< HEAD
     { prop: 'properties.year_built', id: 'yearBuilt', sort: true, label: 'Year Built', },
 >>>>>>> b377ec0... Freeze zoom to column
+=======
+    { prop: 'properties.year_built', id: 'yearBuilt', width: '110px', sort: true, label: 'Year Built', },
+>>>>>>> 79f6486... Set default column widths in pixels
     { prop: 'properties.condition_field', id: 'conditionRating', sort: true, label: 'Condition Rating', }
   );
