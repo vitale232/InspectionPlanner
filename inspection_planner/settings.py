ec2637edc4efc7974851c71de1295f20ac1a3837
simple_scout <vitale232@gmail.com>
2019-12-23 12:55:02 -0500

    PAGE_SIZE: 1000

--- inspection_planner/settings.py
+++ inspection_planner/settings.py
@@ -162,7 +162,7 @@ STATICFILES_DIRS = [
 # Django Rest Framework settings
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 1000
+    'PAGE_SIZE': 1000,
 }
 
 # Have Django enforce some security headers on browsers that support it
