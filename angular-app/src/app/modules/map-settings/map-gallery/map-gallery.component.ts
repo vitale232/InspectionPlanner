ccc2181c21e754aac40ce955673fad8b014b7fe6
simple_scout <vitale232@gmail.com>
2020-01-21 18:26:42 -0500

    Add acknowledgements to leaflet

--- angular-app/src/app/modules/map-settings/map-gallery/map-gallery.component.ts
+++ angular-app/src/app/modules/map-settings/map-gallery/map-gallery.component.ts
@@ -32,7 +32,8 @@ export class MapGalleryComponent implements OnInit {
         'This map provides an auto-generated clustering of the NYSDOT brige locations. The features are clustered ' +
         'based on distance. The map is highly interactive. You can hover over a cluster to see its general shape, and you ' +
         'can click on a cluster to zoom the map\'s extent to the area. When you finally zoom in close enough, you will see ' +
-        'individual markers for each bridge. A popup with attributes is revealed when clicking on a bridge marker.',
+        'individual markers for each bridge. A popup with attributes is revealed when clicking on a bridge marker. ' +
+        'This map is built using Leaflet and Leaflet Markercluster via the Asymmetrik Angular bindings.',
       buttonText: 'Explore',
       link: '/marker-cluster'
     }
