8e13ad0741132e6f195a251a8be3903fd70c7d2d
simple_scout <vitale232@gmail.com>
2020-02-07 17:32:55 -0500

    Change grids mapBridgesDataSource to IBridgeFeature properties rather than feature

--- angular-app/src/app/shared/components/grid-components/attribute-grid/attribute-grid.component.ts
+++ angular-app/src/app/shared/components/grid-components/attribute-grid/attribute-grid.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit, Input, ViewChild } from '@angular/core';
 import { PblNgridColumnSet, PblDataSource, createDS, PblNgridComponent } from '@pebula/ngrid';
 import { Observable } from 'rxjs';
-import { IBridgeFeature } from 'src/app/shared/models/bridges.model';
+import { IBridgeFeature, IBridgeProperties } from 'src/app/shared/models/bridges.model';
 
 import * as config from './columns.config';
+import { filter, map } from 'rxjs/operators';
 
 
 @Component({
@@ -16,8 +17,12 @@ export class AttributeGridComponent implements OnInit {
   @Input() bridges$: Observable<IBridgeFeature[]>;
 
   columns: PblNgridColumnSet = config.COLUMNS.build();
-  mapBridgesDataSource: PblDataSource = createDS<IBridgeFeature>()
-    .onTrigger( () => this.bridges$ )
+
+  mapBridgesDataSource: PblDataSource = createDS<IBridgeProperties>()
+    .onTrigger( () => this.bridges$.pipe(
+      filter(x => x !== null),
+      map(bridgeFeatures => bridgeFeatures.map(bridge => bridge.properties))
+    ))
     .create();
 
   @ViewChild(PblNgridComponent, { static: true }) table: PblNgridComponent<IBridgeFeature>;
